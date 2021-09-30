0b5b6ddb6aefa0c52aa50d27cbc8aac163ab56c4
Messquerade <annac.klingberg@gmail.com>
2021-09-30 13:52:18 -0700

    add Record views new, index
    Co-authored-by: Anthony McRae <toddmac12@gmail.com>

--- MusicOrganizer/Controllers/RecordController.cs
+++ MusicOrganizer/Controllers/RecordController.cs
@@ -7,6 +7,24 @@ namespace MusicOrganizer.Controllers
 {
   public class RecordController : Controller
   {
+    [HttpGet("/record")]
+    public ActionResult Index()
+    {
+      List<Record> allRecords = Record.GetAll();
+      return View(allRecords);
+    }
 
+    [HttpGet("/record/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/record")]
+    public ActionResult Create(string title)
+    {
+      Record newRecord = new Record(title);
+      return RedirectToAction("Index");
+    }
   }
 }
